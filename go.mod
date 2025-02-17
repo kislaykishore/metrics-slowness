df72a013818e9f91f8170e92313bc8fe4c0d440d
Kislay Kishore <kislayk@google.com>
2025-02-17 06:34:20 +0000

    Implement parallelism

--- go.mod
+++ go.mod
@@ -1,3 +1,84 @@
 module OTelPerf
 
 go 1.24
+
+require (
+	github.com/googlecloudplatform/gcsfuse/v2 v2.10.0
+	github.com/prometheus/client_golang v1.20.5
+	go.opentelemetry.io/contrib/detectors/gcp v1.34.0
+	go.opentelemetry.io/otel v1.34.0
+	go.opentelemetry.io/otel/exporters/prometheus v0.56.0
+	go.opentelemetry.io/otel/sdk v1.34.0
+	go.opentelemetry.io/otel/sdk/metric v1.34.0
+)
+
+require (
+	cel.dev/expr v0.19.1 // indirect
+	cloud.google.com/go v0.118.0 // indirect
+	cloud.google.com/go/auth v0.14.0 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.7 // indirect
+	cloud.google.com/go/compute/metadata v0.6.0 // indirect
+	cloud.google.com/go/iam v1.3.1 // indirect
+	cloud.google.com/go/longrunning v0.6.4 // indirect
+	cloud.google.com/go/monitoring v1.22.1 // indirect
+	cloud.google.com/go/storage v1.50.0 // indirect
+	github.com/GoogleCloudPlatform/opentelemetry-operations-go/detectors/gcp v1.25.0 // indirect
+	github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/metric v0.49.0 // indirect
+	github.com/GoogleCloudPlatform/opentelemetry-operations-go/internal/resourcemapping v0.49.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/cncf/xds/go v0.0.0-20241223141626-cff3c89139a3 // indirect
+	github.com/envoyproxy/go-control-plane/envoy v1.32.3 // indirect
+	github.com/envoyproxy/protoc-gen-validate v1.1.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
+	github.com/google/s2a-go v0.1.9 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
+	github.com/googleapis/gax-go/v2 v2.14.1 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
+	github.com/magiconair/properties v1.8.9 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/planetscale/vtprotobuf v0.6.1-0.20240319094008-0393e58bdf10 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.62.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/sagikazarmark/locafero v0.7.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.12.0 // indirect
+	github.com/spf13/cast v1.7.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.19.0 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.58.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.58.0 // indirect
+	go.opentelemetry.io/otel/metric v1.34.0 // indirect
+	go.opentelemetry.io/otel/trace v1.34.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/exp v0.0.0-20250106191152-7588d65b2ba8 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/oauth2 v0.25.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/time v0.9.0 // indirect
+	google.golang.org/api v0.217.0 // indirect
+	google.golang.org/genproto v0.0.0-20250106144421-5f5ef82da422 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250106144421-5f5ef82da422 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250106144421-5f5ef82da422 // indirect
+	google.golang.org/grpc v1.69.4 // indirect
+	google.golang.org/protobuf v1.36.3 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/googlecloudplatform/gcsfuse/v2/common"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.opentelemetry.io/contrib/detectors/gcp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/prometheus"
+	"go.opentelemetry.io/otel/metric"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"golang.org/x/sync/errgroup"
+
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+)
+
+var (
+	fsOpsMeter    = otel.Meter("fs_op")
+	fsOpsCount, _ = fsOpsMeter.Int64Counter("fs/ops_count", metric.WithDescription("The cumulative number of ops processed by the file system."))
+	workers       = flag.Int("workers", 128, "Concurrent workers")
+	timeout       = flag.Duration("timeout", 2*time.Minute, "Time to run the benchmark")
+)
+
+type ShutdownFn func(ctx context.Context) error
+
+// JoinShutdownFunc combines the provided shutdown functions into a single function.
+func JoinShutdownFunc(shutdownFns ...ShutdownFn) ShutdownFn {
+	return func(ctx context.Context) error {
+		var err error
+		for _, fn := range shutdownFns {
+			if fn == nil {
+				continue
+			}
+			err = errors.Join(err, fn(ctx))
+		}
+		return err
+	}
+}
+
+const serviceName = "test"
+const metricsPort = 9181
+
+var allowedMetricPrefixes = []string{"fs/", "gcs/", "file_cache/"}
+
+// SetupOTelMetricExporters sets up the metrics exporters
+func SetupOTelMetricExporters(ctx context.Context) (shutdownFn common.ShutdownFn) {
+	shutdownFns := make([]common.ShutdownFn, 0)
+	options := make([]sdkmetric.Option, 0)
+
+	opts, shutdownFn := setupPrometheus(metricsPort)
+	options = append(options, opts...)
+	shutdownFns = append(shutdownFns, shutdownFn)
+
+	res, err := getResource(ctx)
+	if err != nil {
+		fmt.Printf("Error while fetching resource: %v\n", err)
+	} else {
+		options = append(options, sdkmetric.WithResource(res))
+	}
+
+	options = append(options, sdkmetric.WithView(dropDisallowedMetricsView))
+
+	meterProvider := sdkmetric.NewMeterProvider(options...)
+	shutdownFns = append(shutdownFns, meterProvider.Shutdown)
+
+	otel.SetMeterProvider(meterProvider)
+
+	return common.JoinShutdownFunc(shutdownFns...)
+}
+
+// dropUnwantedMetricsView is an OTel View that drops the metrics that don't match the allowed prefixes.
+func dropDisallowedMetricsView(i sdkmetric.Instrument) (sdkmetric.Stream, bool) {
+	s := sdkmetric.Stream{Name: i.Name, Description: i.Description, Unit: i.Unit}
+	for _, prefix := range allowedMetricPrefixes {
+		if strings.HasPrefix(i.Name, prefix) {
+			return s, true
+		}
+	}
+	s.Aggregation = sdkmetric.AggregationDrop{}
+	return s, true
+}
+
+func setupPrometheus(port int64) ([]sdkmetric.Option, common.ShutdownFn) {
+	if port <= 0 {
+		return nil, nil
+	}
+	exporter, err := prometheus.New(prometheus.WithoutUnits(), prometheus.WithoutCounterSuffixes(), prometheus.WithoutScopeInfo(), prometheus.WithoutTargetInfo())
+	if err != nil {
+		fmt.Printf("Error while creating prometheus exporter:%v\n", err)
+		return nil, nil
+	}
+	shutdownCh := make(chan context.Context)
+	done := make(chan interface{})
+	go serveMetrics(port, shutdownCh, done)
+	return []sdkmetric.Option{sdkmetric.WithReader(exporter)}, func(ctx context.Context) error {
+		shutdownCh <- ctx
+		close(shutdownCh)
+		<-done
+		close(done)
+		return nil
+	}
+}
+
+func serveMetrics(port int64, shutdownCh <-chan context.Context, done chan<- interface{}) {
+	fmt.Printf("Serving metrics at localhost:%d/metrics\n", port)
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+	prometheusServer := &http.Server{
+		Addr:           fmt.Sprintf(":%d", port),
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	go func() {
+		if err := prometheusServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Printf("Failed to start Prometheus server: %v\n", err)
+		}
+	}()
+	go func() {
+		ctx := <-shutdownCh
+		defer func() { done <- true }()
+		fmt.Println("Shutting down Prometheus exporter.")
+		if err := prometheusServer.Shutdown(ctx); err != nil {
+			fmt.Printf("Error while shutting down Prometheus exporter:%v\n", err)
+			return
+		}
+		fmt.Println("Prometheus exporter shutdown")
+	}()
+	fmt.Println("Prometheus collector exporter started")
+}
+
+func getResource(ctx context.Context) (*resource.Resource, error) {
+	return resource.New(ctx,
+		// Use the GCP resource detector to detect information about the GCP platform
+		resource.WithDetectors(gcp.NewDetector()),
+		resource.WithTelemetrySDK(),
+		resource.WithAttributes(
+			semconv.ServiceName(serviceName),
+			semconv.ServiceVersion(common.GetVersion()),
+		),
+	)
+}
+
+func main() {
+	flag.Parse()
+	ctx := context.Background()
+	shutdown := SetupOTelMetricExporters(ctx)
+	defer shutdown(ctx)
+	ctx, cancelFunc := context.WithDeadline(ctx, time.Now().Add(*timeout))
+	defer cancelFunc()
+	g, ctx := errgroup.WithContext(ctx)
+	for i := 0; i < *workers; i++ {
+		g.Go(func() error {
+			for {
+				select {
+				case <-ctx.Done():
+					return nil
+				default:
+					fsOpsCount.Add(ctx, 1)
+				}
+			}
+		})
+	}
+	if err := g.Wait(); err != nil {
+		fmt.Printf("Error: %v", err)
+	}
+
+	fmt.Println("Sleeping for 30 seconds. Please capture metrics")
+	<-time.After(30 * time.Second)
+}
